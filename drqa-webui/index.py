c6a45b41d7bba511c10f30ea1064cb39195e903a
Ankur3107 <ankur310794@gmail.com>
2020-01-29 17:00:31 +0530

    chatbot ui

---
+++ drqa-webui/index.py
@@ -0,0 +1,16 @@
+import json
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+#from services import DrQA, process
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/query", methods=["POST"])
+def query():
+    data = request.json
+    #answers = process(question=data['question'])
+    answers = data['question']
+    return json.dumps(answers)
